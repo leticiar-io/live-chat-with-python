b0b4c82bd20c91de98f0c3d6be9ab3179740ee1c
leticiar-io <leticia.rodrigues20019@gmail.com>
2024-05-10 17:05:45 -0300

    feat: página incial e popup

--- main.py
+++ main.py
@@ -20,11 +20,36 @@ import flet as ft
 # 2. Criar a função principal do seu aplicativo
 def main(pagina):
    # criar funcionalidades 
-   titulo = ft.Text("Bem-vindo(a) ao ZapZap!")
-   botao_iniciar = ft.ElevatedButton("Iniciar o chat")
+   titulo = ft.Text("ZapZap")
+
+   #popup
+   titulo_janela = ft.Text("Bem-vindo ao ZapZap!")
+   campo_nome_usuario = ft.TextField(label="Escreva seu nome no chat")
+
+   def iniciar_chat(evento):
+      pagina.remove(titulo)
+      pagina.remove(botao_iniciar)
+      janela.open = False
+      
+      pagina.update()
+
+   botao_enviar = ft.ElevatedButton("Entrar no chat", on_click=iniciar_chat)
+   janela = ft.AlertDialog(
+      title=titulo_janela, 
+      content=campo_nome_usuario, 
+      actions=[botao_enviar]
+   )
+
+   def iniciar_chat(evento):
+      pagina.dialog = janela
+      janela.open = True
+      pagina.update()
+
+   botao_iniciar = ft.ElevatedButton("Iniciar o chat", on_click=iniciar_chat)
+
    pagina.add(titulo)
    pagina.add(botao_iniciar)
 
 
 # 3. Rodar o aplicativo
-ft.app(main)
+ft.app(main, view=ft.WEB_BROWSER)
